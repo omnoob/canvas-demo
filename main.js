fd1793ac09fc221a5e398d72b2b6418b5fb755aa
omnoob <896265004@qq.com>
2019-03-03 23:16:35 +0800

    触屏设备兼容

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var context = yyy.getContext('2d');
 
 autoSetCanvasSize(yyy)
 
-listenToMove(yyy)
+listenToUser(yyy)
 
 var eraserEnalbed = false
 eraser.onclick = function () {
@@ -50,32 +50,64 @@ function drawLine(x1, y1, x2, y2) {
     context.closePath()
 }
 
-function listenToMove(canvas) {
+function listenToUser(canvas) {
     var using = false
     var lastPoint = { "x": undefined, "y": undefined }
-    canvas.onmousedown = function (aaa) {
-        var x = aaa.clientX
-        var y = aaa.clientY
-        using = true
-        if (eraserEnalbed) {
-            context.clearRect(x - 5, y - 5, 10, 10) // 清除区域是正方形，xy各-5，鼠标就在清除区域的正中心
-        } else {
-            lastPoint = { "x": x, "y": y }
+    //特性检测
+    if(document.ontouchstart !== undefined){
+        // 触屏设备
+        canvas.ontouchstart = function(aaa){
+            var x = aaa.touches[0].clientX
+            var y = aaa.touches[0].clientY
+            using = true
+            if (eraserEnalbed) {
+                context.clearRect(x - 5, y - 5, 10, 10) // 清除区域是正方形，xy各-5，鼠标就在清除区域的正中心
+            } else {
+                lastPoint = { "x": x, "y": y }
+            }
+        }
+        canvas.ontouchmove = function(aaa){
+            var x = aaa.touches[0].clientX
+            var y = aaa.touches[0].clientY
+            if (!using) { return }
+            if (eraserEnalbed) {
+                context.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                var newPoint = { "x": x, "y": y }
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint  // 注意理解这行
+            }
+        }
+        canvas.ontouchend = function(aaa){
+            using = false
+        }
+    }else{
+        // 非触屏设备
+        canvas.onmousedown = function (aaa) {
+            var x = aaa.clientX
+            var y = aaa.clientY
+            using = true
+            if (eraserEnalbed) {
+                context.clearRect(x - 5, y - 5, 10, 10) // 清除区域是正方形，xy各-5，鼠标就在清除区域的正中心
+            } else {
+                lastPoint = { "x": x, "y": y }
+            }
+        }
+        canvas.onmousemove = function (aaa) {
+            var x = aaa.clientX
+            var y = aaa.clientY
+            if (!using) { return }
+            if (eraserEnalbed) {
+                context.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                var newPoint = { "x": x, "y": y }
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint  // 注意理解这行
+            }
+        }
+        canvas.onmouseup = function (aaa) {
+            using = false
         }
     }
-    canvas.onmousemove = function (aaa) {
-        var x = aaa.clientX
-        var y = aaa.clientY
-        if (!using) { return }
-        if (eraserEnalbed) {
-            context.clearRect(x - 5, y - 5, 10, 10)
-        } else {
-            var newPoint = { "x": x, "y": y }
-            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-            lastPoint = newPoint  // 注意理解这行
-        }
-    }
-    canvas.onmouseup = function (aaa) {
-        using = false
-    }
+    
 }
